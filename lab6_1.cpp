042a5083085180a03261172842762c02a7d80f35
suphaloek66 <suphaleok_khueanphet@cmu.ac.th>
2023-12-01 15:48:49 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int i = 1;
+    int e = 0;
+    int o = 0;
+    while ( i != 0){
+        cout << "Enter an integer: ";
+        cin >> i;
+        if ( i%2 == 0 and i != 0 ){
+            e = e+1;
+        }
+        else if( i%2 != 0){
+            o = o+1;
+        }
+    }
+    cout << "#Even numbers = "<< e << endl;
+    cout << "#Odd numbers = "<< o << endl;
     return 0;
 }
